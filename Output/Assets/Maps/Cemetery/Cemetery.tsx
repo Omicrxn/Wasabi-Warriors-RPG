d8f788e64e15950e73fd2612c753b9f32d2abb83
mav006 <59050192+mav006@users.noreply.github.com>
2021-03-24 20:06:25 +0100

    Cemetery

---
+++ Output/Assets/Maps/Cemetery/Cemetery.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Cemetery" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Japanese Bundle/Graveyard/Tilemaps/RPGMakerVXAce/GK_FG_B_2.png" width="512" height="512"/>
+</tileset>
---
+++ Output/Assets/Maps/Cemetery/Cemetery2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Cemetery2" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="Japanese Bundle/Graveyard/Tilemaps/RPGMakerVXAce/GK_FG_A5_2.png" width="256" height="512"/>
+</tileset>
