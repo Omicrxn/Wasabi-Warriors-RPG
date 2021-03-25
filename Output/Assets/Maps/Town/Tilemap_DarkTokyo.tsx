b685997fa27c58167f866bc812a4a69850051c79
Omicrxn <alexavila0128@gmail.com>
2021-03-25 20:05:12 +0100

    Tilesets are now embedded and maps in xml instead of csv

--- Output/Assets/Maps/Town/Tilemap_DarkTokyo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Tilemap" tilewidth="32" tileheight="32" tilecount="1050" columns="42">
- <image source="Tilemap.png" width="1344" height="800"/>
-</tileset>
