8ad5e0b00af6060bf6626d0b6eef3ad9c24f96a8
mav006 <59050192+mav006@users.noreply.github.com>
2021-05-28 13:01:21 +0200

    Gui Adjust

--- Game/Source/ScreenPause.cpp
+++ Game/Source/ScreenPause.cpp
@@ -26,17 +26,17 @@ ScreenPause::ScreenPause()
 	kenzoAnim.PushBack({ 795, 519, 162, 186 });
 	kenzoAnim.PushBack({ 987, 519, 162, 186 });
 	kenzoAnim.loop = true;
-	kenzoAnim.speed = 0.01f;
+	kenzoAnim.speed = 0.004f;
 
 	eikenAnim.PushBack({ 795, 732, 162, 192 });
 	eikenAnim.PushBack({ 987, 732, 162, 192 });
 	eikenAnim.loop = true;
-	eikenAnim.speed = 0.005f;
+	eikenAnim.speed = 0.004f;
 
 	reiAnim.PushBack({ 795, 308, 162, 188 });
 	reiAnim.PushBack({ 987, 308, 162, 188 });
 	reiAnim.loop = true;
-	reiAnim.speed = 0.005f;
+	reiAnim.speed = 0.004f;
 
 	state = MobileState::MAIN;
 }
@@ -76,17 +76,17 @@ bool ScreenPause::Load(int minIndex, int maxIndex, Scene* currentScene, Window* 
 	iconExit->state = GuiControlState::HIDDEN;
 	++counterId;
 
-	iconQuest = (GuiIcon*)guiManager->CreateGuiControl(GuiControlType::ICON, counterId, { 490, position + 400, 90, 90 });
+	iconQuest = (GuiIcon*)guiManager->CreateGuiControl(GuiControlType::ICON, counterId, { 490, position + 435, 90, 90 });
 	iconQuest->SetIconProperties(currentScene, atlas[0], font, hoverFx, clickFx, CONTROLLER_BUTTON_START, IconType::ICON_QUEST);
 	iconQuest->state = GuiControlState::HIDDEN;
 	++counterId;
 
-	iconTeam = (GuiIcon*)guiManager->CreateGuiControl(GuiControlType::ICON, counterId, { 590, position + 400, 90, 90 });
+	iconTeam = (GuiIcon*)guiManager->CreateGuiControl(GuiControlType::ICON, counterId, { 595, position + 435, 90, 90 });
 	iconTeam->SetIconProperties(currentScene, atlas[0], font, hoverFx, clickFx, CONTROLLER_BUTTON_START, IconType::ICON_TEAM);
 	iconTeam->state = GuiControlState::HIDDEN;
 	++counterId;
 
-	iconMap = (GuiIcon*)guiManager->CreateGuiControl(GuiControlType::ICON, counterId, { 690, position + 400, 90, 90 });
+	iconMap = (GuiIcon*)guiManager->CreateGuiControl(GuiControlType::ICON, counterId, { 710, position + 435, 90, 90 });
 	iconMap->SetIconProperties(currentScene, atlas[0], font, hoverFx, clickFx, CONTROLLER_BUTTON_START, IconType::ICON_MAP);
 	iconMap->state = GuiControlState::HIDDEN;
 
@@ -135,7 +135,7 @@ bool ScreenPause::Draw(Render* render)
 			if (currentAnimation != nullptr)
 			{
 				currentAnimation->Update();
-				render->DrawTexture(atlas[0], 500, position + 120, &currentAnimation->GetCurrentFrame(), 0.0f);
+				render->DrawTexture(atlas[0], 500, position + 135, &currentAnimation->GetCurrentFrame(), 0.0f);
 			}
 		}
 	}
@@ -150,18 +150,18 @@ bool ScreenPause::Draw(Render* render)
 		char s[24] = { 0 };
 
 		// Gold HUD
-		render->DrawText(menuFont, "$:", 683, position + 123, 50, 3, { 0,0,0,255 });
-		render->DrawText(menuFont, "$:", 680, position + 120, 50, 3, { 249,215,28,255 });
+		render->DrawText(menuFont, "$:", 688, position + 143, 50, 3, { 0,0,0,255 });
+		render->DrawText(menuFont, "$:", 685, position + 140, 50, 3, { 249,215,28,255 });
 		sprintf_s(s, 24, "%i", docNode.attribute("gold").as_int());
-		render->DrawText(menuFont, s, 763, position + 123, 50, 3, { 0,0,0,255 });
-		render->DrawText(menuFont, s, 760, position + 120, 50, 3, { 255,255,255,255 });
+		render->DrawText(menuFont, s, 763, position + 143, 50, 3, { 0,0,0,255 });
+		render->DrawText(menuFont, s, 760, position + 140, 50, 3, { 255,255,255,255 });
 
 		// XP HUD
-		render->DrawText(menuFont, "XP:", 683, position + 163, 50, 3, { 0,0,0,255 });
-		render->DrawText(menuFont, "XP:", 680, position + 160, 50, 3, { 60,179,113,255 });
+		render->DrawText(menuFont, "XP:", 688, position + 188, 50, 3, { 0,0,0,255 });
+		render->DrawText(menuFont, "XP:", 685, position + 185, 50, 3, { 60,179,113,255 });
 		sprintf_s(s, 24, "%i", docNode.attribute("xp").as_int());
-		render->DrawText(menuFont, s, 763, position + 163, 50, 3, { 0,0,0,255 });
-		render->DrawText(menuFont, s, 760, position + 163, 50, 3, { 255,255,255,255 });
+		render->DrawText(menuFont, s, 763, position + 188, 50, 3, { 0,0,0,255 });
+		render->DrawText(menuFont, s, 760, position + 185, 50, 3, { 255,255,255,255 });
 	}
 	else if (state == MobileState::QUEST)
 	{
@@ -174,12 +174,19 @@ bool ScreenPause::Draw(Render* render)
 			if (entityManager->playerList.At(i)->data->IsActive())
 			{
 				// Draw player name
-				render->DrawText(menuFont, entityManager->playerList.At(i)->data->name.GetString(), 500, position + 50, 30, 3, { 0,0,0,255 });
+				render->DrawText(menuFont, entityManager->playerList.At(i)->data->name.GetString(), 588, position + 53, 50, 3, { 255,165,0,255 });
 
 				// Draw player description from the wiki
+				int offsetA = 20;
 				if (entityManager->playerList.At(i)->data->name == "Kenzo")
 				{
-					render->DrawText(menuFont, "blablablabalbal", 500, position + 90, 15, 3, { 0,0,0,255 });
+					render->DrawText(menuFont, "Young man from Osaka with passion", 490, position + 90 + offsetA, 19, 3, { 0,0,0,255 });
+					render->DrawText(menuFont, "for cooking, but no resources.", 490, position + 105 + offsetA, 19, 3, { 0,0,0,255 });
+					render->DrawText(menuFont, "Then he found Erika, the owner of the", 490, position + 120 + offsetA, 19, 3, { 0,0,0,255 });
+					render->DrawText(menuFont, "best restaurant in the region, who ", 490, position + 135 + offsetA, 19, 3, { 0,0,0,255 });
+					render->DrawText(menuFont, "gave Kenzo an opportunity.", 490, position + 150 + offsetA, 19, 3, { 0,0,0,255 });
+					render->DrawText(menuFont, "Now Kenzo is a grateful cook in her", 490, position + 165 + offsetA, 19, 3, { 0,0,0,255 });
+					render->DrawText(menuFont, "restaurant in Otaru.", 490, position + 180 + offsetA, 19, 3, { 0,0,0,255 });
 				}
 				else if (entityManager->playerList.At(i)->data->name == "Eiken")
 				{
@@ -191,40 +198,41 @@ bool ScreenPause::Draw(Render* render)
 				}
 
 				// Draw Player Stats
-				render->DrawText(font, "LVL:", 500, 210, 15, 2, { 255,255,255,255 });
-				render->DrawText(font, "Damage:", 500, 250, 15, 2, { 255,255,255,255 });
-				render->DrawText(font, "Max HP:", 500, 290, 15, 2, { 255,255,255,255 });
-				render->DrawText(font, "HP:", 500, 330, 15, 2, { 255,255,255,255 });
-				render->DrawText(font, "Strength:", 500, 370, 15, 2, { 255,255,255,255 });
-				render->DrawText(font, "Defense:", 500, 410, 15, 2, { 255,255,255,255 });
-				render->DrawText(font, "Atk Speed:", 500, 450, 15, 2, { 255,255,255,255 });
-				render->DrawText(font, "CritRate:", 500, 490, 15, 2, { 255,255,255,255 });
+				int offsetB = 50;
+				render->DrawText(font, "LVL:", 500, 290 + offsetB, 18, 2, { 255,165,0,255 });
+				render->DrawText(font, "Damage:", 500, 310 + offsetB, 18, 2, { 255,165,0,255 });
+				render->DrawText(font, "Max HP:", 500, 330 + offsetB, 18, 2, { 255,165,0,255 });
+				render->DrawText(font, "HP:", 500, 350 + offsetB, 18, 2, { 255,165,0,255 });
+				render->DrawText(font, "Strength:", 500, 370 + offsetB, 18, 2, { 255,165,0,255 });
+				render->DrawText(font, "Defense:", 500, 390 + offsetB, 18, 2, { 255,165,0,255 });
+				render->DrawText(font, "Atk Speed:", 500, 410 + offsetB, 18, 2, { 255,165,0,255 });
+				render->DrawText(font, "CritRate:", 500, 430 + offsetB, 18, 2, { 255,165,0,255 });
 
-				int posStatsNum = 600;
+				int posStatsNum = 630;
 				char statsString[30] = { 0 };
 				sprintf_s(statsString, 30, "%i  ", entityManager->playerList.At(i)->data->stats.level);
-				render->DrawText(font, statsString, posStatsNum, 210, 15, 2, { 255,255,255,255 });
+				render->DrawText(font, statsString, posStatsNum, 290 + offsetB, 18, 2, { 0,0,0,255 });
 
 				sprintf_s(statsString, 30, "%i  ", entityManager->playerList.At(i)->data->stats.damage);
-				render->DrawText(font, statsString, posStatsNum, 250, 15, 2, { 255,255,255,255 });
+				render->DrawText(font, statsString, posStatsNum, 310 + offsetB, 18, 2, { 0,0,0,255 });
 
 				sprintf_s(statsString, 30, "%i  ", entityManager->playerList.At(i)->data->stats.maxHP);
-				render->DrawText(font, statsString, posStatsNum, 290, 15, 2, { 255,255,255,255 });
+				render->DrawText(font, statsString, posStatsNum, 330 + offsetB, 18, 2, { 0,0,0,255 });
 
 				sprintf_s(statsString, 30, "%i  ", entityManager->playerList.At(i)->data->stats.currentHP);
-				render->DrawText(font, statsString, posStatsNum, 330, 15, 2, { 255,255,255,255 });
+				render->DrawText(font, statsString, posStatsNum, 350 + offsetB, 18, 2, { 0,0,0,255 });
 
 				sprintf_s(statsString, 30, "%i  ", entityManager->playerList.At(i)->data->stats.strength);
-				render->DrawText(font, statsString, posStatsNum, 370, 15, 2, { 255,255,255,255 });
+				render->DrawText(font, statsString, posStatsNum, 370 + offsetB, 18, 2, { 0,0,0,255 });
 
 				sprintf_s(statsString, 30, "%i  ", entityManager->playerList.At(i)->data->stats.defense);
-				render->DrawText(font, statsString, posStatsNum, 410, 15, 2, { 255,255,255,255 });
+				render->DrawText(font, statsString, posStatsNum, 390 + offsetB, 18, 2, { 0,0,0,255 });
 
 				sprintf_s(statsString, 30, "%i  ", entityManager->playerList.At(i)->data->stats.attackSpeed);
-				render->DrawText(font, statsString, posStatsNum, 450, 15, 2, { 255,255,255,255 });
+				render->DrawText(font, statsString, posStatsNum, 410 + offsetB, 18, 2, { 0,0,0,255 });
 
 				sprintf_s(statsString, 30, "%.1f  ", entityManager->playerList.At(i)->data->stats.criticalRate);
-				render->DrawText(font, statsString, posStatsNum, 490, 15, 2, { 255,255,255,255 });
+				render->DrawText(font, statsString, posStatsNum, 430 + offsetB, 15, 2, { 0,0,0,255 });
 			}
 		}
 	}
@@ -260,9 +268,9 @@ void ScreenPause::Enable()
 	easing->CreateSpline(&iconExit->bounds.y, 108, 1500, SplineType::QUINT);
 	easing->CreateSpline(&iconResume->bounds.y, 580, 1500, SplineType::QUINT);
 
-	easing->CreateSpline(&iconQuest->bounds.y, 400, 1500, SplineType::QUINT);
-	easing->CreateSpline(&iconTeam->bounds.y, 400, 1500, SplineType::QUINT);
-	easing->CreateSpline(&iconMap->bounds.y, 400, 1500, SplineType::QUINT);
+	easing->CreateSpline(&iconQuest->bounds.y, 435, 1500, SplineType::QUINT);
+	easing->CreateSpline(&iconTeam->bounds.y, 435, 1500, SplineType::QUINT);
+	easing->CreateSpline(&iconMap->bounds.y, 435, 1500, SplineType::QUINT);
 
 	Screen::Enable();
 }
@@ -275,9 +283,9 @@ void ScreenPause::Disable()
 	iconExit->bounds.y = position + 108;
 	iconResume->bounds.y = position + 580;
 
-	iconQuest->bounds.y = position + 400;
-	iconTeam->bounds.y = position + 400;
-	iconMap->bounds.y = position + 400;
+	iconQuest->bounds.y = position + 435;
+	iconTeam->bounds.y = position + 435;
+	iconMap->bounds.y = position + 435;
 	Screen::Disable();
 }
 
