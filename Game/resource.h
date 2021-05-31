a0146b844cf789dbacd3df12f7052aeaa80d391e
Omicrxn <alexavila0128@gmail.com>
2021-05-31 22:30:58 +0200

    Icon added and metadata written

--- Game/resource.h
+++ Game/resource.h
@@ -3,12 +3,13 @@
 // Used by Game.rc
 //
 #define IDI_ICON1                       107
+#define IDI_ICON2                       108
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
